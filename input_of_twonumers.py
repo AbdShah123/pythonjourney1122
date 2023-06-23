136fdfd8f26a10195e14f18acd9fd369cd73ed8d
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-23 14:59:46 +0500

    MySecondtrycode
    just revising the entire lesson altogether

---
+++ input_of_twonumers.py
@@ -0,0 +1,16 @@
+# program to calculate input of 2 numbers and give result 
+# the numbers are predeteermined
+a=3
+b=5
+c=a+b
+print("this is your result "+str(int(c)))
+# the next program take input from the user and print the result instead of pre-determined answer
+d=input("Please input the first number ")
+e=input("Please input the second number ")
+f=int(str(d))+int(str(e))
+print("this is your result "+str(int(f)))
+# important thing to know that python is case sensitive language
+# meaning if I write A it'll not overwrite a
+A="Snippets"
+B="useful"
+print(A+" are quite "+B)
