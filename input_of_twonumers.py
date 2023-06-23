8c213453795bb4d6f96c87520084ff2176554fca
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-23 15:17:40 +0500

    Update input_of_twonumers.py

--- input_of_twonumers.py
+++ input_of_twonumers.py
@@ -13,4 +13,22 @@ print("this is your result "+str(int(f)))
 # meaning if I write A it'll not overwrite a
 A="Snippets"
 B="useful"
-print(A+" are quite "+B)
+print(str(A)+" are quite "+str(B))
+# re-explaining of the code through variable print demonstration
+print(f"Variables are {A} and {B}")
+if type(A)==str:
+    print("the type of data is string type")
+else:
+    print("the type of data is integer ")
+if type(B)==str:
+    print("the Variable is a type of string data ")
+else:
+    print("the type of data is integer ")
+print("the type of data is "+str(type (B)))
+#unpacking a list using variables
+friuts=["apple","cherry","bannana"]
+x,y,z=friuts
+print(x)
+print(y)
+print(z)
+
