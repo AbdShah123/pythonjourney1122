cdb2627ddd18740315110823fbc8b155b5e8431e
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 12:36:07 +0500

    boolean statement applying for first time

---
+++ condtionalstatementbasicone.py
@@ -0,0 +1,10 @@
+# i am now testing boolean operators now
+condition1= True
+condition2=False
+print(not condition1)
+print(condition1 and condition2)
+print(condition1 or condition2)
+print(not condition1 or condition2)
+print(not condition1 and condition2)
+# so this is the demonstration of boolean statement now in the next one we will learn about the 
+# different logical operator
