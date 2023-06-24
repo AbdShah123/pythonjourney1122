00b54784a67944f9bb1bdcfc9a4608a6cf73084a
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 16:21:37 +0500

    final commit of this code

--- testingifvaluesexistbyin.py
+++ testingifvaluesexistbyin.py
@@ -5,3 +5,16 @@ print(3/2) #using single division symbol will gave you a floating point number
 print(3//2) #using multiple division symbol will allow you to gave result in single whole point number
 print(3**2)  #rememeber it's exponen toperator represented by two asterik as you can see by the result
 # augmented operator also known as increment  or decrement operator as you can see the difference with the following code 
+x=3
+y=1
+y=y+x
+print(y)         #difficult way to solve the problem
+z=1
+z+=y
+print(z)
+z/=y
+print(z)
+z*=y
+print(z)              #meanwhile z taking a dip in various versions of reality
+z%=y                  #observe the answer and see the difference between ordinary and complicated way to solve
+print(z)
