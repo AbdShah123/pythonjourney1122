7fd805fe5fcf7d45b149b93316ea7b8d1af8578f
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 16:15:47 +0500

    still work in progress

---
+++ testingifvaluesexistbyin.py
@@ -0,0 +1,7 @@
+course_2="this is python programming language"
+print("python" in course_2)       # to test wheather the required course or courses exist within python
+# it will give you answer of true as you can see from the result
+print(3/2) #using single division symbol will gave you a floating point number
+print(3//2) #using multiple division symbol will allow you to gave result in single whole point number
+print(3**2)  #rememeber it's exponen toperator represented by two asterik as you can see by the result
+# augmented operator also known as increment  or decrement operator as you can see the difference with the following code 
