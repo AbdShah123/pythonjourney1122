cc1a31ff043908d0702cbac76aaa57b57838343d
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 15:51:44 +0500

    doind some basic stupid shit

--- stringobjectsorfunctions.py
+++ stringobjectsorfunctions.py
@@ -1,11 +1,11 @@
 # now we will learn string object in python
-string_1=''
+string_1='HelloWorld'
 string_1.upper()             #if I print this it'll return me the result in the upper case and vice versa 
 # now let's print this result in upper case
 print(string_1.upper())    #similarly performing operations like this one
-print(string_1.split())    #presummely just split the words nothing uncommmon rigth over here 
+print(string_1.split(","))    #presummely just split the words nothing uncommmon rigth over here 
 print(string_1.format())
 width=10
 print(string_1.center(width))
 print(string_1.ljust(width))
-print(string_1.casefold())
+print(string_1.casefold())        #doesn't change the original string only return a new string
