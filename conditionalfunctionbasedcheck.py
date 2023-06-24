87204d7dd980434e8f95a16c6749e97dec5f1b35
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 13:03:51 +0500

    onceagainnewupdatedcode

--- conditionalfunctionbasedcheck.py
+++ conditionalfunctionbasedcheck.py
@@ -1,8 +1,13 @@
-def condtion(statement1,statement2):
+def condtion():
+   while True:
+    statement1=int(input("Please input the first number: "))
+    statement2=int(input("Please input the second number: "))
     if statement1>statement2:
-        print(f"{statement1} is the bigger value")
+       print(f"{statement1} is the bigger one")
     else:
-        print(f"{statement2} is the bigger value")
-    return 
-        
-input=condtion(int(),int())
+       print(f"{statement2} is the bigger one")
+    c=input("do you want to perfom the calculation again (Y/N) :")
+    if c=='N' or c=='n':
+            break
+       
+condtion()
