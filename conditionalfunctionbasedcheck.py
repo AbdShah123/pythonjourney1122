7459f0d030e18fb987ee86c0c0226270092bf30f
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-24 13:03:19 +0500

    newupdatedcode

---
+++ conditionalfunctionbasedcheck.py
@@ -0,0 +1,8 @@
+def condtion(statement1,statement2):
+    if statement1>statement2:
+        print(f"{statement1} is the bigger value")
+    else:
+        print(f"{statement2} is the bigger value")
+    return 
+        
+input=condtion(int(),int())
