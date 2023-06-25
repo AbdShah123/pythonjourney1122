75a93972c30912bffee5cb41e352861ea4eba744
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-25 15:01:18 +0500

    task number two point two

---
+++ study/tasknumbertwopointtwo.py
@@ -0,0 +1,23 @@
+name=input("Please input your name: ")
+print(f"Hello {name} Nice to meet you would you like to learn some python today")
+# first task complete
+print(f"{name.lower()}")
+print(f"{name.upper()}")
+print(f"{name.title()}")
+#second task complete
+print("Albert Einstien once said:\n\"A Person who has never made a mistake has never tried anything new\"")
+#3rd task complete
+print("task # 4")
+famous_person="Albert Einstien"
+message=print(f"{famous_person} once said:\n\"A Person who has never made a mistake has never tried anything new\"")
+print(message)
+#task number 4 complete
+first_name=input("enter your first name: ")
+last_name=input("enter your last name: ")
+print(f"{first_name}\t {last_name}")
+print(f"{first_name}\n {last_name}")
+print(f"{first_name.lstrip()} {last_name.lstrip()}")
+print(f"{first_name.rstrip()} {last_name.rstrip()}")
+print(f"{first_name.strip()} {last_name.strip()}")
+#fifth task complete
+
