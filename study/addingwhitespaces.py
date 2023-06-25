531189c23afa532ea19475f5c88dadd3277849d2
AbdShah123 <87001854+AbdShah123@users.noreply.github.com>
2023-06-25 12:35:13 +0500

    secondcommit

--- study/addingwhitespaces.py
+++ study/addingwhitespaces.py
@@ -2,3 +2,5 @@ var="hello world"
 print(var.rstrip())      #remove any whitespace if available
 print("hello\nworld")    # add text to next line
 print("Hi!\tThere")      #consective spaces to each text
+print(var)               #it's only temporary so the text will reapper itself once more
+print(var.lstrip())      #removing whitespaces fro left side
